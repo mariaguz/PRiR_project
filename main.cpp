f39943d4f7558367abd2cf383f766c4cb974ff45
Pawel <plpasza@gmail.com>
2022-05-10 16:44:04 +0200

    fixes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <omp.h>
 #include "mpi.h" 
 #include <iostream>
-#include <ctime>
-#include <stdexcept>
 #include <cstdlib>
-#include <string>
 #include <algorithm>
 #include <chrono>
 #include <locale.h>
 
-omp_lock_t lock;
-
 #define THREADS 4
-#define SIZE 3e6
+#define SIZE 1e6
 #define RANGE 10000
 #define LB 2000
 #define UB 5000
@@ -30,206 +25,56 @@ double* generate_data(int n, int low, int high) {
 	return data;
 }
 
-double* split_data(double* data, int begin, int end) {
-
-	int range = end - begin;
-
-	if (begin > end) {
-		std::cout << "\n(Split_data) ERROR: Begin>end.\n";
-		return NULL;
-	}
-	else {
-		double* splitted_data = new double[range];
-		int j = 0;
-
-		for (int i = begin; i < end; i++) {
-			splitted_data[j] = data[i];
-			j++;
-		}
-
-		return splitted_data;
-	}
-	
-}
-
-/*
-double computeAVG_indexes(int n, double* data, int first, int last) {
-	auto beginTime = std::chrono::high_resolution_clock::now();
-
-	float avg = 0;
-	int counter = 0;
-
-	if (first < 0 || last >= n) {
-		std::cout << "\n(Srednia z zakresu indeksow) ERROR: Indeksy poza zakresem.\n";
-		return -1;
-	}
-	else {
-		if (first > last) {
-			std::cout << "\n(Srednia z zakresu indeksow) ERROR: Indeks 'last' jest mniejszy od 'first'\n";
-			return -1;
-		}
-		else {
-			for (int i = first; i <= last; i++) {
-				avg += data[i];
-				counter++;
-			}
-
-			avg = avg / counter;
-
-			std::cout << "\nSrednia (zakres indeksow) = " << avg;
-			auto endTime = std::chrono::high_resolution_clock::now();
-			auto time = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime);
-
-			printf("\nCzas: %f\n", time.count() * 1e-3);
-			return avg;
-		}
-	}
-
-	
-
-
-}
-*/
-
-double* computeAVG_values(int n, double* data, int val_min, int val_max) {
-
-	auto beginTime = std::chrono::high_resolution_clock::now();
-	
-	double avg = 0, sum = 0;
-	int counter = 0;
-
-	float min = *std::min_element(data, data + n);
-	float max = *std::max_element(data, data + n);
-	//std::cout << "\nMin = " << min << "\nMax = " << max;
-
-	if (val_min < min || val_max > max) {
-		std::cout << "\n(Srednia z zakresu wartosci) ERROR: Wartosci poza zakresem.\n";
-		return NULL;
-	}
-	else {
-		if (val_min > val_max) {
-			std::cout << "\n(Srednia z zakresu wartosci) ERROR: Wartosc min > max.\n";
-			return NULL;
-		}
-		else {
-			for (int i = 0; i < n; i++) {
-				if (data[i] >= val_min && data[i] <= val_max) {
-					sum += data[i];
-					counter++;
-				}
-			}
-
-			avg = sum / counter;
-//			std::cout << "\nSrednia (zakres wartosci) = " << avg;
-
-			auto endTime = std::chrono::high_resolution_clock::now();
-			auto time = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime);
-
-			double* results = new double[4];
-
-			results[0] = avg;
-			results[1] = sum;
-			results[2] = counter;
-			results[3] = time.count() * 1e-3;
-
-			return results;
-		}
-	}
-
-}
-
-double* generate_data(int n, int low, int high) {
-	double* data = new double[n];
-	for (int i = 0; i < n; i++) {
-		data[i] = ((double)(rand() % 10000) / 10000.0) * (high - low) + low;
-	}
-	return data;
-}
-
-double* split_data(double* data, int begin, int end) {
-	int size = end - begin;
-	double* subset = new double[size];
-	for (int i = 0; i < size; i++)
-	{
-		subset[i] = data[i + begin];
-	}
-	return subset;
-}
-
 int main(int argc, char** argv) {
 
     setlocale (LC_ALL,"");
-    omp_init_lock(&lock);
-	int rank, size, tag;
+	int rank, size, tag, i;
     double* data;
 	tag = 0;
 	rank = 0;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-
+//
+//
     if (rank == 0) {
         std::cout << "Watki " << THREADS << "\tProcesy " << size;
-		////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = sequence = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-		data = generate_data(SIZE, 0, RANGE);
+        ////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = sequence = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+        data = generate_data(SIZE, 0, RANGE);
 
-		double* results_seq = computeAVG_values(SIZE, data, LB, UB);
+        double avg_seq, sum_seq = 0;
+        int count_seq = 0;
 
-		//Wypisanie rezultatów
-		printf("\nCzas Sekwencyjne: %f", results_seq[3]);
-		printf("\nAVG Sekwencyjne: %.2f", results_seq[0]);
+        auto beginTime = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < SIZE; i++) {
+            if (data[i] >= LB && data[i] <= UB) {
+                sum_seq += data[i];
+                count_seq++;
+            }
+        }
+        avg_seq = sum_seq / count_seq;
+        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::high_resolution_clock::now() - beginTime);
+        //Wypisanie rezultatów
+        printf("\nCzas Sekwencyjne: %f", time.count() * 1e-3);
+        printf("\nAVG Sekwencyjne: %.2f", avg_seq);
 
 
+		////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = OpenMP = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+		int array_size = SIZE;
+        double sum_OpenMP = 0, avg_OpenMP;
+        int count_OpenMP = 0;
+        auto beginTime_OMP = std::chrono::high_resolution_clock::now();
 
-
-
-		////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = OpenMP = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
-		//double* data = generate_data(SIZE, 0, 1000);
-		double* sums_OpenMP = new double[THREADS];
-		double* counts_OpenMP = new double[THREADS];
-
-
-
-		auto beginTime = std::chrono::high_resolution_clock::now();
-
-		omp_set_num_threads(THREADS);
-
-#pragma omp parallel
-		{
-			//pobranie ID wątku
-			int tread_num = omp_get_thread_num();
-
-			// obliczenie przedziałów dla których srednie beda liczone rownolegle
-			int begin = round(double(SIZE / THREADS) * tread_num);
-			int end = round(double(SIZE / THREADS) * tread_num + SIZE / THREADS);
-			int subsize = end - begin;
-
-			//podział zbioru
-			double* splitted_data = new double[subsize];
-			splitted_data = split_data(data, begin, end);
-
-			//obliczenie wyników w każdym wątku
-			double* results = computeAVG_values(subsize, splitted_data, LB, UB);
-
-			sums_OpenMP[tread_num] = results[1];
-			counts_OpenMP[tread_num] = results[2];
-
-		}
-
-		//obliczenie średniej ogólnej
-		double sum_OpenMP = 0;
-		double count_OpenMP = 0;
-
-		for (int i = 0; i < THREADS; i++) {
-			sum_OpenMP = sum_OpenMP + sums_OpenMP[i];
-			count_OpenMP = count_OpenMP + counts_OpenMP[i];
-		}
-		double avg_OpenMP = sum_OpenMP / count_OpenMP;
-
-        auto endTime = std::chrono::high_resolution_clock::now();
-		auto time = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime);
-
+#pragma omp parallel for num_threads(THREADS) reduction(+:sum_OpenMP) reduction(+:count_OpenMP)
+        for (i = 0; i < array_size; ++i) {
+            if (data[i] >= LB && data[i] <= UB) {
+                sum_OpenMP += data[i];
+                count_OpenMP++;
+            }
+        }
+        avg_OpenMP = sum_OpenMP / count_OpenMP;
+        time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - beginTime_OMP);
 		//Wypisanie rezultatów
 		printf("\nCzas OpenMP: %f", time.count() * 1e-3);
 		printf("\nAVG OpenMP: %.2f", avg_OpenMP);
@@ -237,12 +82,24 @@ int main(int argc, char** argv) {
 	////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = MPI = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 
 	MPI_Barrier(MPI_COMM_WORLD);
-	double start = MPI_Wtime();
-	int count = SIZE/(size-1);
-	int rest = SIZE - count * (size-1);
+	int count = SIZE/(size), rest = SIZE - count * (size);
+	double sum_MPI = 0, cnt_MPI = 0, avg_MPI;
 	double * local_array;
+    double sum = 0;
+    double count_in_tread = 0;
+    double* results = new double[2];
+    double end;
+    int cnt;
+
+    double start = MPI_Wtime();
+
 	if (rank == 0) {
-	    int cnt;
+        for (i = 0; i < count; ++i) {
+            if (data[i] >= LB && data[i] <= UB) {
+                sum += data[i];
+                count_in_tread++;
+            }
+        }
         for(int dest = 1; dest < size; ++dest)
         {
             if(dest < size - 1) {
@@ -257,7 +114,6 @@ int main(int argc, char** argv) {
 	}
 
 	if (rank != 0) {
-	    int cnt;
 	    if(rank == size - 1){
             cnt = count + rest;
 	        local_array = new double [cnt];
@@ -266,26 +122,30 @@ int main(int argc, char** argv) {
 	        local_array = new double [cnt];
 	    }
         MPI_Recv(local_array, cnt, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	    double* results = computeAVG_values(cnt, local_array, LB, UB);
-        MPI_Send(&results[0], 4, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+        for (i = 0; i < cnt; ++i) {
+            if (local_array[i] >= LB && local_array[i] <= UB) {
+                sum_MPI += local_array[i];
+                cnt_MPI++;
+            }
+        }
+        results[0] = sum_MPI;
+        results[1] = cnt_MPI;
+        MPI_Send(&results[0], 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
 
 
 	if (rank == 0) {
-	    double sum = 0;
-	    double count_in_tread = 0;
         for(int src = 1; src < size; ++src)
         {
-            double* results = new double[4];
-            MPI_Recv(results, 4, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            sum += results[1];
-            count_in_tread += results[2];
+            MPI_Recv(results, 2, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            sum += results[0];
+            count_in_tread += results[1];
         }
 
-        double avg_MPI = sum / count_in_tread;
-        double end = MPI_Wtime();
+        avg_MPI = sum / count_in_tread;
+        end = MPI_Wtime();
         printf("\nCzas MPI: %f", end - start);
         printf("\nAVG MPI: %.2f", avg_MPI);
     }
@@ -293,9 +153,17 @@ int main(int argc, char** argv) {
     ////= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = HYBRID = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
     MPI_Barrier(MPI_COMM_WORLD);
+
+	double sum_hybrid = 0, cnt_hybrid = 0;
+
     start = MPI_Wtime();
     if (rank == 0) {
-        int cnt;
+        for (i = 0; i < count; ++i) {
+            if (data[i] >= LB && data[i] <= UB) {
+                sum += data[i];
+                count_in_tread++;
+            }
+        }
         for(int dest = 1; dest < size; ++dest)
         {
             if(dest < size - 1) {
@@ -310,7 +178,6 @@ int main(int argc, char** argv) {
     }
 
     if (rank != 0) {
-        int cnt;
         if(rank == size - 1){
             cnt = count + rest;
             local_array = new double [cnt];
@@ -320,57 +187,35 @@ int main(int argc, char** argv) {
         }
         MPI_Recv(local_array, cnt, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        omp_set_num_threads(THREADS);
-        double* sums_hyb = new double[THREADS];
-        double* counts_hyb = new double[THREADS];
-#pragma omp parallel
-        {
-            //pobranie ID wątku
-            int tread_num = omp_get_thread_num();
-
-            // obliczenie przedziałów dla których srednie beda liczone rownolegle
-            int begin = round(double(cnt / THREADS) * tread_num);
-            int end = round(double(cnt / THREADS) * tread_num + cnt / THREADS);
-            int subsize = end - begin;
-
-            //podział zbioru
-            double* splitted_data = split_data(local_array, begin, end);
-
-            //obliczenie wyników w każdym wątku
-            double* results = computeAVG_values(subsize, splitted_data, LB, UB);
-
-            sums_hyb[tread_num] = results[1];
-            counts_hyb[tread_num] = results[2];
-
+#pragma omp parallel for num_threads(THREADS) reduction(+:sum_hybrid) reduction(+:cnt_hybrid)
+        for (i = 0; i < cnt; ++i) {
+            if (local_array[i] >= LB && local_array[i] <= UB) {
+                sum_hybrid += local_array[i];
+                cnt_hybrid++;
+            }
         }
 
-        //obliczenie średniej ogólnej
-        double results[2] = {0};
-        for (int i = 0; i < THREADS; i++) {
-            results[0] += sums_hyb[i];
-            results[1] += counts_hyb[i];
-        }
-
+        results[0] = sum_hybrid;
+        results[1] = cnt_hybrid;
         MPI_Send(&results[0], 2, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 
     }
-    MPI_Barrier(MPI_COMM_WORLD)
+    MPI_Barrier(MPI_COMM_WORLD);
+
 
     if (rank == 0) {
-        double sum = 0;
-        double count_in_tread = 0;
         for(int src = 1; src < size; ++src)
         {
-            double* results = new double[4];
+
             MPI_Recv(results, 2, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             sum += results[0];
             count_in_tread += results[1];
         }
 
-        double avg_hyb = sum / count_in_tread;
-        double end = MPI_Wtime();
+        avg_MPI = sum / count_in_tread;
+        end = MPI_Wtime();
         printf("\nCzas HYBRID: %f", end - start);
-        printf("\nAVG HYBRID: %.2f", avg_hyb);
+        printf("\nAVG HYBRID: %.2f", avg_MPI);
     }
 
     MPI_Finalize();
